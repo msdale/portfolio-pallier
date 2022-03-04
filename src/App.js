05950feeb29c88d473f4123a7cd642e5c08dd56c
Mark Dale <msdaledad@gmail.com>
2022-03-04 10:37:40 -0500

    initial setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        This is the HEADER
+      </header>
+    </div>
+  );
+}
+
+export default App;
